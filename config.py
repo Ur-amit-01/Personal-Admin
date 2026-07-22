85680db46064ea3b6cbf60ee65521c904bc9a27e
Amit <heroku467@gmail.com>
2026-07-22 10:32:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ API_HASH = os.environ.get("API_HASH", "5b0e07f5a96d48b704eb9850d274fe1d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8113642693:AAG9yJpZjyhKIP_nhsIIoc8ZsiTJ-gsudLU") 
 DB_NAME = os.environ.get("DB_NAME","amit")     
 DB_URL = os.environ.get("DB_URL","mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '2031106491').split()]
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMINS', '2031106491').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '2031106491 7150972327').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMINS', '2031106491 7150972327').split()]
 PORT = os.environ.get("PORT", "8080")
 
-RESTART_NOTIFICATION = False
+RESTART_NOTIFICATION = True
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', False)) # Set True or False
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True)) # Set True or False
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
